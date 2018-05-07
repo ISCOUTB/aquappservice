5b64df2cabcbd4051373a80275120c9f1a89f0be
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2018-05-07 02:14:16 -0500

    Implementing frontend usint Angular 4

--- api/aquapp_api/views.py
+++ api/aquapp_api/views.py
@@ -5,3 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from aquapp_api import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
