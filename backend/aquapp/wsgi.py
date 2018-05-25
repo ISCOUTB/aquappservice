04fc1bf66ae00382b0ba24f52de920ff39e63a14
Jairo Serrano <jairo.serrano@gmail.com>
2018-05-25 18:28:56 -0500

    incomplete migration to nginx for backend

---
+++ backend/aquapp/wsgi.py
@@ -0,0 +1,10 @@
+"""
+This script runs the aquapp application using a development server.
+"""
+from dotenv import load_dotenv  # Loading app settings from the .env file
+print('Loading environment variables')
+load_dotenv(dotenv_path='/backend/aquapp/.env', verbose=True)
+from aquapp.api import app as application
+
+if __name__ == '__main__':
+    application.run()
