e52045760b16b25c05e717dcf177b82effad423c
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2018-05-26 11:11:11 -0500

    After hours of dreadful sorrow I finally managed to get uwgsi and nginx configured for the backend, but now the icampff values are not being calculated, working on it.

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,7 +4,11 @@ This script runs the aquapp application using a development server.
 from dotenv import load_dotenv  # Loading app settings from the .env file
 print('Loading environment variables')
 load_dotenv(dotenv_path='/backend/aquapp/.env', verbose=True)
-from .api import app
+
+from aquapp.apis.core.database import Database
+Database().seed()
+
+from aquapp.api import app
 
 if __name__ == '__main__':
     app.run()
