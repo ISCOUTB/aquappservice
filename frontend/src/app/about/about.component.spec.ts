7bc774734433444a0334eeee717f12ad320e1bef
ArashiJF <juanfmf2209@hotmail.com>
2018-12-07 02:32:59 -0500

    export cant find pipe acronym, about didnt solve by importing CommonMod

--- frontend/src/app/about/about.component.spec.ts
+++ frontend/src/app/about/about.component.spec.ts
@@ -2,10 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateService } from '../translate/translate.service';
 import { AboutComponent } from './about.component';
-import { MatMenuModule, MatToolbarModule, MatCardModule, MatGridListModule, MatTableModule, MatTabGroup, MatTab, MatTabHeader, MatTabBody } from '@angular/material';
+import { MatMenuModule, MatToolbarModule, MatCardModule, MatGridListModule, MatTableModule, MatTabGroup, MatTab, MatTabHeader, MatTabBody, MatNativeDateModule, MatDatepickerModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatSnackBarModule, MatDialogModule, MatDividerModule, MatTabsModule } from '@angular/material';
 import { TranslatePipe } from '../translate/translate.pipe';
-import { RouterTestingModule } from '@angular/router/testing';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
  
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -21,13 +22,23 @@ describe('AboutComponent', () => {
         MatTabHeader,
         MatTabBody 
       ],
-      imports : [ MatIconModule,
+      imports : [
+        BrowserAnimationsModule,
+        MatIconModule,
         MatMenuModule,
         MatToolbarModule,
         MatCardModule,
         MatGridListModule,
         MatTableModule,
-        RouterTestingModule,
+        MatDividerModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
         CommonModule
       ],
       providers : [ TranslateService ]
--- frontend/src/app/export/export.component.spec.ts
+++ frontend/src/app/export/export.component.spec.ts
@@ -2,8 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateService } from '../translate/translate.service';
 import { TranslatePipe } from '../translate/translate.pipe';
 import { ExportComponent } from './export.component';
-import { MatIconModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatIconModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatCheckboxModule, MatSelectModule, MatRadioButton, MatRadioModule, MatProgressSpinnerModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('ExportComponent', () => {
   let component: ExportComponent;
@@ -16,8 +17,14 @@ describe('ExportComponent', () => {
         MatIconModule,
         MatDatepickerModule,
         MatNativeDateModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatProgressSpinnerModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CommonModule
       ],
       providers: [
         TranslateService
