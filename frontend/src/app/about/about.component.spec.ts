f8535ae3cb1ed5fa8f02bedbef73f26e4d2f20db
ArashiJF <juanfmf2209@hotmail.com>
2018-12-07 03:00:13 -0500

    about works now, everything is green good to go

--- frontend/src/app/about/about.component.spec.ts
+++ frontend/src/app/about/about.component.spec.ts
@@ -2,11 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateService } from '../translate/translate.service';
 import { AboutComponent } from './about.component';
-import { MatMenuModule, MatToolbarModule, MatCardModule, MatGridListModule, MatTableModule, MatTabGroup, MatTab, MatTabHeader, MatTabBody, MatNativeDateModule, MatDatepickerModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatSnackBarModule, MatDialogModule, MatDividerModule, MatTabsModule } from '@angular/material';
+import { MatMenuModule, MatToolbarModule, MatCardModule, MatGridListModule, MatTableModule, MatTabGroup, MatTab, MatTabHeader, MatTabBody, MatNativeDateModule, MatDatepickerModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatSnackBarModule, MatDialogModule, MatDividerModule, MatTabsModule, MatRippleModule } from '@angular/material';
 import { TranslatePipe } from '../translate/translate.pipe';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpHandler } from '@angular/common/http';
  
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -16,32 +17,19 @@ describe('AboutComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AboutComponent, 
-        TranslatePipe,
-        MatTabGroup,
-        MatTab,
-        MatTabHeader,
-        MatTabBody 
+        TranslatePipe 
       ],
       imports : [
-        BrowserAnimationsModule,
         MatIconModule,
         MatMenuModule,
         MatToolbarModule,
+        MatSelectModule,
         MatCardModule,
         MatGridListModule,
-        MatTableModule,
-        MatDividerModule,
-        MatDialogModule,
-        MatSnackBarModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormsModule,
-        MatSelectModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        CommonModule
+        MatTabsModule,
+        BrowserAnimationsModule
       ],
-      providers : [ TranslateService ]
+      providers : [ TranslateService, HttpClient, HttpHandler ]
     })
     .compileComponents();
   }));
