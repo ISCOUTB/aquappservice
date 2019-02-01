3fa38767df72d5ca2f1b6020b7be1594c4bd9ff2
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-01-31 19:49:38 -0500

    Login form

--- aquapp/src/app/login/login.component.ts
+++ aquapp/src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import {
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
+} from '@angular/animations';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    animations: [
+        trigger('fadeIn', [
+            transition(':enter', [
+                style({ opacity: '0', height: 0 }),
+                animate('0.5s ease-out', style({ opacity: '1', height: '*' })),
+            ]),
+        ]),
+    ],
 })
 export class LoginComponent implements OnInit {
+    user = '';
+    password = '';
 
-  constructor() { }
+    constructor(
+        private apiService: ApiService
+    ) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    authenticate() {
+      this.apiService.login(this.user, this.password);
+    }
 }
