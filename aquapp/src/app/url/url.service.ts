0624b220dca389ff03fb0a6ccc72679bc56ffcbd
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-05 20:36:30 -0500

    Url service to create urls with query params across the app

---
+++ aquapp/src/app/url/url.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+    UrlTree,
+    UrlSegmentGroup,
+    DefaultUrlSerializer,
+    UrlSegment,
+} from '@angular/router';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UrlService {
+    constructor() {}
+
+    gen(segments: string[], queryParams: any): string {
+        const urlTree = new UrlTree();
+        urlTree.root = new UrlSegmentGroup(
+            segments.map(segment => new UrlSegment(segment, {})),
+            {}
+        );
+        urlTree.queryParams = queryParams;
+        const urlSerializer = new DefaultUrlSerializer();
+        return urlSerializer.serialize(urlTree);
+    }
+}
