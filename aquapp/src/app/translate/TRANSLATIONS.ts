c43614c0aecaae5ce42bb45446fadba3743a22c5
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-09 09:36:11 -0500

    About component fully translated

--- aquapp/src/app/translate/TRANSLATIONS.ts
+++ aquapp/src/app/translate/TRANSLATIONS.ts
@@ -405,5 +405,245 @@ export const TRANSLATIONS = [
   {
     en: 'Close navigation menu',
     es: 'Cerrar menú de navegación'
+  },
+  {
+    en: 'Donate',
+    es: 'Donar'
+  },
+  {
+    en: 'Special thanks',
+    es: 'Agradecimientos especiales'
+  },
+  {
+    en:
+      'Special thanks to Invemar for their technical support for the estimation of the ICAMpff through the direct connection to their API.',
+    es:
+      'Gracias a Invemar por brindarnos soporte técnico para el cálculo del ICAMpff a través de la conexión directa a su API'
+  },
+  {
+    en: 'Project',
+    es: 'Proyecto'
+  },
+  {
+    en: 'Features',
+    es: 'Características'
+  },
+  {
+    en: 'Real data',
+    es: 'Datos reales'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    en: `AquApp is a web-based platform created to carry out the monitoring and subsequent follow-up of some physical-chemical and biological parameter of water quality of the estuarine bayous and lagoons internal system (or simply lagoon system) of Cartagena de Indias. The platform also calculates`,
+    // tslint:disable-next-line:max-line-length
+    es: `AquApp es una plataforma creada para llevar a cabo el monitoreo y seguimiento de algunos parámetros fisicoquímicos y biológicos de calidad de agua para el sistema interno de caños y lagunas (o sistema lagunar) de Cartagena de Indias. La plataforma calcula, además,`
+  },
+  {
+    en:
+      ' the Marine and Estuarine Water Quality Index for Fauna and Flora Preservation ',
+    es:
+      ' el Indicador de la Calidad de Aguas Marinas y Estuarinas para Preservación de Flora y Fauna '
+  },
+  {
+    en:
+      '(ICAMpff, by its initials in Spanish), which was created by Invemar (Institute for Marine and Coastal Research).',
+    es:
+      '(ICAMpff) creado por el Invemar (Instituto de Investigaciones Marinas y Costeras).'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    en: `The data herein is presented in two different type of format: plots and tables (files in “.csv”). The user may: (a) visualize the chart in the platform and (b) compare any other parameter from any of the other monitoring points located along the lagoon system.`,
+    // tslint:disable-next-line:max-line-length
+    es: `Los datos en la plataforma están disponibles en dos tipos de formato: gráfica (figura) y tabulado (archivo con extensión ".csv"). El usuario puede visualizar las gráficas en la plataforma y, adicionalmente, comparar, en la misma gráfica, datos de cualquiera de los puntos de monitoreo establecidos a lo largo del sistema lagunar.`
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Data is collected by UTB undergraduate students through both a multiparameter probe and water samples, which are then sent to the lab. Monitoring is performed once a week, and the data for some of the parameters are expected -in a second stage of the project- to be remotely sensed so as to have real time information',
+    es: `La información es recolectada por estudiantes no graduados de la UTB a través de una sonda multi parámetro y muestras de agua.
+         El monitoreo es realizado una vez por semana, y la recolección de algunos de los datos se espera que sea remota
+         (en una segunda etapa del proyecto).`
+  },
+  {
+    en: `Filtering`,
+    es: `Filtrado`
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Available data can be filtered by selecting a station type, date range, station, and parameter using the form on the bottom toolbar and the floating controls',
+    es: `La información disponible puede ser filtrada eligiendo un tipo de estación, rango de fechas
+         y parámetro usando la barra de tareas en la parte inferior y los contorles flotantes`
+  },
+  {
+    en: 'Download',
+    es: `Descarga`
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'After having filtered the data, the information is available either as a Comma Separated Values (.csv) file or as a graph, which may be also visualized in the platform.',
+    es: `Luego de filtrar la información, esta puede ser descargada en el formato de texto separado por comas (csv)
+         o como un gráfico.`
+  },
+  {
+    en: 'No restrictions',
+    es: `Sin restricciones`
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Unlike other institutions with monitoring plans on water bodies of the internal bayous and lagoons system of Cartagena that restrict access to the data taken, Aquapp provides information available for the entire community',
+    es: `A diferencia de otras instituciones con planes de monitoreo de los cuerpos de
+         agua de Cartagena, los cuales restringen el acceso a la informacióń obtenida,
+         AquApp deja su información disponible para toda la comunidad.`
+  },
+  {
+    en: 'Cool',
+    es: `Atractivo`
+  },
+  {
+    en:
+      'To guarantee a great experience, we developed a well-built and up-to-date website with an interface thought for the users.',
+    // tslint:disable-next-line:max-line-length
+    es: `Para garantizar una gran experiencia, desarrollamos un sitio web bien construído y al día, con contenido útil y una interfaz pensada para los usuarios.`
+  },
+  {
+    en: 'Cross Browser',
+    es: `Multi navegador`
+  },
+  {
+    en:
+      'The website is built to be compatible with all modern browsers (Chrome, Firefox, Safari, and Opera). Feel free to try it yourself!.',
+    es: `Este sitio fue construído de modo que sea compatible con todos los
+         navegadores modernos (Puedes usarlo en )Chrome, Firefox, Safari y Opera).
+         ¡Siéntete libre de probarlo por ti mismo!.`
+  },
+  {
+    en: 'Technologies used',
+    es: `Tecnologías usadas`
+  },
+  {
+    en: 'Developer',
+    es: `Desarrollador`
+  },
+  {
+    en: 'Researcher',
+    es: `Investigador`
+  },
+  {
+    en: 'Information',
+    es: `Información`
+  },
+  {
+    en:
+      'AquApp is a web-based platform that supports the monitoring program of the bayous and lagoons system of Cartagena de Indias.',
+    es: `AquApp es una plataforma que soporta un programa de monitoreo del sistema de caños y lagunas de Cartagena de Indias.`
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'This program is freely available: you may redistribute and/or modify it under the terms of the license GNU Affero General Public License or any other subsequent version.',
+    es: `Este programa es software libre: usted puede resdistribuírlo y/o
+         modificarlo bajo los términos de la licencia GNU Affero General Public License, u
+         opcionalemente cualquier versión posterior.`
+  },
+  {
+    en: 'For more information, click ',
+    es: `Para más información haga clic `
+  },
+  {
+    en: 'here',
+    es: `aquí`
+  },
+  {
+    en: ' to read the GNU Affero General Public License.',
+    es: ` para leer la licencia GNU Affero General Public License.`
+  },
+  {
+    en: 'Contact',
+    es: `Contacto`
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'You are free to type your comments in our project\'s Wiki, where we may provide you with additional in-depth information about the project.',
+    // tslint:disable-next-line:max-line-length
+    es: `Usted es libre de escribirnos agregando un mensaje en nuestra wiki. En esta wiki (alojada en Github) podemos además brindarle información a fondo sobre el proyecto.`
+  },
+  {
+    en: 'Team',
+    es: 'Equipo'
+  },
+  {
+    en: 'Join the conversation',
+    es: 'Únase a la conversación'
+  }, // About component Bios
+  {
+    // Laura
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Laura is a Computer Engineer gratuated with Summa Cum Laude Honors from Universidad Tecnológica de Bolívar (Cartagena, COL). Due to her excellent academic performance, she won a honor tuition each semester during her undergraduate studies, and also was beneficiary of the program “Becas Iberoamérica. Estudiantes de Grado. Santander Universidades” of Santander Bank for an academic exchange at the Instituto Tecnológico y de Estudios Superiores de Monterrey (Mexico City, MX) in 2015. While studying she worked on a comparative study of different clustering algorithms for estimating evaluated groups that share similar conceptual weaknesses for her undergraduate thesis. She also works as a full-stack developer with knowledge in technologies such as HTML5, CSS3, JavaScript, Laravel 5.0+, MySQL & MongoDB, Django among others, which has allowed her to be a co-author of RaosAPI used for the control and registration of students’ attendance to classes as well as AquApp and AquApp 2.0 platforms. She is currently doing a MSc in Computer Science and Engineering at the Politecnico di Milano, Italy with an emphasis on Artificial Intelligence.',
+    es:
+      // tslint:disable-next-line:max-line-length
+      'Laura es una ingeniera de sistemas egresada Summa Cum Laude de la Universidad Tecnológica de Bolívar (2017). Gracias a su excelente desempeño académico, fue merecedora de matrícula de honor cada semestre durante sus estudios de pregrado, y también fue beneficiaria del programa  “Becas Iberoamérica. Estudiantes de Grado. Santander Universidades” de Banco Santander para realizar un intercambio académico en el Instituto Tecnológico y de Estudios Superiores de Monterrey (Ciudad de México, MX). Durante su paso por la universidad participó en un estudio comparativo de diferentes algoritmos de clustering para la estimación de grupos de evaluados que comparten debilidades conceptuales similares. Además se desempeña como desarrolladora full-stack con conocimiento en tecnologías como HTML5, CSS3, JavaScript, Laravel 5.0+, MySQL, MongoDB, Django entre otras, lo que le ha permitido ser coautora de RaosAPI para el control de asistencia de estudiantes a clase, así como de las plataformas AquApp y AquApp 2.0. en conjunto con la universidad. Actualmente realiza una maestría en Ciencias de la Computación e Ingeniería en el Politécnico de Milán, Italia, con énfasis en Inteligencia Artificial.'
+  },
+  {
+    // Juan Carlos
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Juan Carlos is an Electronic Engineer and Master in Electrical Power from the Industrial University of Santander. Doctor from the University of Northeastern, Boston. Fellow Fulbright-DNP-Colciencias 2007. He has been a researcher and teacher at the Technological University of Bolivar since 2004. He obtained the title of Full Professor in 2017. His research work has focused on architecture and computer organization. The main areas of application of their projects include: hardware support for improvements in computer security, architectures with multiple cores and multiple processors, and advanced digital design techniques (hardware description languages, intellectual property modules, programmable systems, embedded systems , and co-design hardware / software). Since 2012 he has headed an interdisciplinary working group with a central axis in ICT. Professor at the Faculty of Engineering where he is in charge of Architecture and Assembler courses (Systems Engineering), Microprocessors (Electronic Engineering), Microcontrollers (Mechatronics Engineering), and Advanced Digital Design Techniques (Master in Engineering, emphasis Ing. Electronics). He is currently in Research License at Ormuco Inc. (Montreal, CA).',
+    es:
+      // tslint:disable-next-line:max-line-length
+      'Juan Carlos es ingeniero Electrónico y magíster en Potencia Eléctrica de la Universidad Industrial de Santander. Doctor de la Universidad de Northeastern, Boston. Becario Fulbright‐DNP‐Colciencias 2007. Ha sido investigador y docente en la Universidad Tecnológica de Bolívar desde el 2004. Obtuvo la categoría de Docente Titular en 2017. Su trabajo de investigación se ha centrado en arquitectura y organización de computadores. Las principales áreas de aplicación de sus proyectos incluyen: soporte hardware para mejoras en la seguridad informática, arquitecturas con múltiples núcleos y múltiples procesadores, y técnicas avanzadas de diseño digital (lenguajes de descripción de hardware, módulos de propiedad intelectual, sistemas programables, sistemas embebidos, y co‐diseño hardware/software). Desde el 2012 dirige un grupo de trabajo interdisciplinario con eje central las TIC. Docente de la Facultad de Ingenierías donde tiene a cargo los cursos de Arquitectura y Ensamblador (Ing. Sistemas), Microprocesadores (Ing. Electrónica), Microcontroladores (Ing. Mecatrónica), y Técnicas Avanzadas de Diseño Digital (Maestría en Ingenierías, énfasis Ing. Electrónica). Actualmente se encuentra en Licencia de Investigación en Ormuco Inc. (Montreal, CA).'
+  },
+  {
+    // Juan Pablo
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Juan Pablo is a student of Computer Engineer at Universidad Tecnológica de Bolívar (Cartagena, Colombia), beneficiary of the program "Ser pilo paga". He participated as a programmer of the current version of AquApp thanks to his expertise with Python, Flask, Javascript, HTML, CSS, Flask, Angular and Docker.',
+    es:
+      // tslint:disable-next-line:max-line-length
+      'Juan Pablo es un estudiante de Ingeniería de Sistemas en la Universidad Tecnológica de Bolívar, beneficiario del programa "Ser pilo paga". Él pudo participar como un programador de la actual versión de AquApp gracias a su experiencia con Python, Javascript, HTML, CSS, Flask, Angular y Docker.'
+  },
+  {
+    // Jairo
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Jairo Serrano, a Computer Engineer (2006) graduated from Universidad Tecnológica de Bolívar (UTB), masters degree in Open Software (UOC) with emphasis in software development (2008). He worked as the Coordinator of Technologies of the Virtual Education Management of the UTB (2001-2010) and a full-time professor of the Faculty of Technical and Technological Studies (2011-2013), currently the Director of the Computer Engineering program of the Engineering Faculty.',
+    es:
+      // tslint:disable-next-line:max-line-length
+      'Jairo Serrano, Ingeniero de sistemas (2006) de la Universidad Tecnológica de Bolívar (UTB), Magíster en Software Libre de la UOC con énfasis en desarrollo de software (2008). Se desempeñó como coordinador de tecnologías de la dirección de educación virtual de la UTB (2001-2010) y profesor de tiempo completo de la Facultad de estudios técnicos y tecnológicos (2011-2013), actualmente director de programa de Ingeniería de Sistemas de la facultad de Ingeniería.'
+  },
+  {
+    en:
+      // tslint:disable-next-line:max-line-length
+      'Víctor is an environmental engineering student at Universidad Tecnologica de Bolivar. Responsible for the monitoring and collection of all samples in the lagoon system for water quality analysis.',
+    es:
+      // tslint:disable-next-line:max-line-length
+      'Víctor es un estudiante de ingeniería ambiental en la universidad Tecnológica de Bolívar.  Encargado del monitoreo y recolección de todas las muestras en el sistema lagunar para el análisis de calidad del agua.'
+  },
+  {
+    en: 'Data monitoring and collection',
+    es: 'Monitoreo y recolección de datos'
+  },
+  {
+    en:
+      'What is the internal system of estuarine bayous and lagoons of Cartagena de Indias?',
+    es: '¿Qué es el sistema interno de caños y lagunas de Cartagena de Indias?'
+  },
+  {
+      en: 'Why AquApp?',
+      es: '¿Por qué AquApp?'
+  },
+  {
+      // tslint:disable-next-line:max-line-length
+      en: 'The lagoon system is considered an intertidal estuarine system. It is home to fauna and flora that depend on the mangrove, which are sensitive to changes in several physical-chemical and biological parameters (pH, temperature, salinity, nitrates, electrical conductivity, dissolved oxygen, feacal and total coliforms, just to mention some) that may or may not favor the survival of permanent and temporary species. Plus, its ecosystemic services may be also compromised: hydrological regime regulation, carbon dioxide removal, erosion control, microclimate and nutrients regulation, solids retention, among others. As one may see, the water quality of this lagoon system must be continuously monitored and followed-up and AquApp makes that possible. The best part: it is freely available for all users!',
+      // tslint:disable-next-line:max-line-length
+      es: 'El sistema lagunar es considerado un ecosistema estuarino intramareal boscoso. Alberga, a lo largo de su recorrido, fauna y flora que dependen de los manglares, los cuales son muy sensibles a los cambios en los valores de diferentes parámetros fisicoquímicos y microbiológicos (pH, temperatura, salinidad, nitratos, conductividad eléctrica, oxígeno disuelto, coliformes totales y fecales, entre otros) que pueden o no favorecer a la supervivencia de las especies permanentes y temporales de este ecosistema; además de, potencialmente, afectar los servicios ecosistémicos que el manglar ofrece (regulación del régimen hidrológico, remoción de dióxido de carbono, control de erosión, regulación de nutrientes y el microclima, hábitat de especies, retención de sólidos, etc.). En consecuencia, la calidad del agua (medio que sostiene el ecosistema) y su continuo monitoreo y seguimiento se hace primordial y AquApp hace posible dicho seguimiento de manera gratuita para todos los usuarios.'
+  },
+  {
+    en: 'Additional information',
+    es: 'Información adicional'
   }
 ];
