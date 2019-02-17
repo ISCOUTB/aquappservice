15ccbec26b8c5f0a6945c0bf497e11b54e37813d
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-17 11:28:50 -0500

    Sensor documentation

--- api/src/controllers/sensor.controller.ts
+++ api/src/controllers/sensor.controller.ts
@@ -6,6 +6,16 @@ import {authenticate, AuthenticationBindings} from '@loopback/authentication';
 import {Sensor} from '../models';
 
 export class SensorController {
+  /**
+   * @param nodeTypeRepository Node type repository
+   * @param sensorRepository Sensor repository
+   * @param user Information about the currently authenticated
+   *  user (if any). This information is provided by the callback function
+   *  in the method verifyBearer of the class AuthStrategyProvider in
+   *  auth-strategy.provider.ts (the payload of the token).
+   *  The second parameter makes it possible to have unprotected methods
+   *  like getElemets.
+   */
   constructor(
     @repository(NodeTypeRepository)
     public nodeTypeRepository: NodeTypeRepository,
@@ -14,6 +24,12 @@ export class SensorController {
     private user: {name: string; id: string; token: string; type: number},
   ) {}
 
+  /**
+   * Get a page of sensors
+   * @param nodeTypeId Id of the node type of the sensors
+   * @param pageIndex Page number, 0 to N
+   * @param pageSize Number of elements
+   */
   @get('/sensors')
   async getElements(
     @param.query.string('nodeTypeId') nodeTypeId: string,
@@ -39,6 +55,10 @@ export class SensorController {
     };
   }
 
+  /**
+   * Creates a new sensor
+   * @param body New sensor
+   */
   @authenticate('BearerStrategy', {type: -1})
   @post('/sensors')
   async newElement(@requestBody() body: Sensor) {
@@ -48,6 +68,10 @@ export class SensorController {
       .then(() => Promise.resolve({}), () => Promise.reject({}));
   }
 
+  /**
+   * Deletes a sensor by its id
+   * @param id Id of the sensor to be deleted
+   */
   @authenticate('BearerStrategy', {type: -1})
   @del('/sensors')
   async delElement(@param.query.string('id') id: string) {
@@ -56,6 +80,11 @@ export class SensorController {
       .then(() => Promise.resolve({}), () => Promise.reject({}));
   }
 
+  /**
+   * Replaces all sensor properties with new data,
+   * except for its id.
+   * @param body New sensor data
+   */
   @authenticate('BearerStrategy', {type: -1})
   @put('/sensors')
   async editElement(
