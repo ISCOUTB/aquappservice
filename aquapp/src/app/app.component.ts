11c05ad6d397600d17c0d4d174280e672e6de13f
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-18 08:39:35 -0500

    Google analytics

--- aquapp/src/app/app.component.ts
+++ aquapp/src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { TranslateService } from './translate/translate.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'aquapp';
 
-  constructor(private translateService: TranslateService) {}
+  constructor(
+    private translateService: TranslateService,
+    private router: Router
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
 
   reloadFiguresInOverview() {
     this.translateService.reload('reload');
