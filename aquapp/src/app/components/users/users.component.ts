5e0f63289f67fb934437d7928ad7347a5d1f952c
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-03-22 19:52:05 -0500

    Apply better animations

--- aquapp/src/app/components/users/users.component.ts
+++ aquapp/src/app/components/users/users.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {
-    animate,
-    state,
-    style,
-    transition,
-    trigger,
+  animate,
+  state,
+  style,
+  transition,
+  trigger
 } from '@angular/animations';
 import { MatPaginator, MatSort } from '@angular/material';
 import { merge, of as observableOf } from 'rxjs';
@@ -15,207 +15,208 @@ import { ApiService } from 'src/app/services/api/api.service';
 import { MessageService } from 'src/app/services/message/message.service';
 
 @Component({
-    selector: 'app-users',
-    templateUrl: './users.component.html',
-    styleUrls: ['./users.component.scss'],
-    animations: [
-        trigger('detailExpand', [
-            state(
-                'collapsed',
-                style({ height: '0px', minHeight: '0', display: 'none' })
-            ),
-            state('expanded', style({ height: '*' })),
-            transition(
-                'expanded <=> collapsed',
-                animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
-            ),
-        ]),
-        trigger('newExpand', [
-            state(
-                'collapsed',
-                style({ height: '0px', minHeight: '0', display: 'none' })
-            ),
-            state('expanded', style({ height: '*' })),
-            transition(
-                'expanded <=> collapsed',
-                animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
-            ),
-        ]),
-    ],
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      )
+    ]),
+    trigger('newExpand', [
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', display: 'none', opacity: 0 })
+      ),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('525ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      )
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: '0', height: 0 }),
+        animate('0.5s ease-out', style({ opacity: '1', height: '*' }))
+      ])
+    ])
+  ]
 })
 export class UsersComponent implements OnInit {
-    displayedColumns: string[] = ['name', 'enabled'];
-    data: User[];
-    resultsLength = 0;
+  displayedColumns: string[] = ['name', 'enabled'];
+  data: User[];
+  resultsLength = 0;
 
-    expandedElement: User;
-    expandedUserName: string;
-    expandedUserPassword: string;
-    expandedUserEmail: string;
-    expandedUserEnabled: boolean;
-    expandedUserRealName: string;
-    expandedUserRealLastName: string;
-    expandedUserType: string;
-    editting = false;
+  expandedElement: User;
+  expandedUserName: string;
+  expandedUserPassword: string;
+  expandedUserEmail: string;
+  expandedUserEnabled: boolean;
+  expandedUserRealName: string;
+  expandedUserRealLastName: string;
+  expandedUserType: string;
+  editting = false;
 
-    toBeDeleted: User;
+  toBeDeleted: User;
 
-    creatingNewElement = false;
+  creatingNewElement = false;
 
-    isLoadingResults = true;
-    pageSize = 10;
+  isLoadingResults = true;
+  pageSize = 10;
 
-    newUserName = 'ejemplo';
-    newUserPassword = 'ejemplo';
-    newUserEmail = 'ejemplo@falso.com';
-    newUserRealName = 'ejemplo';
-    newUserRealLastName = 'falso';
-    newUserType = 'admin';
+  newUserName = 'ejemplo';
+  newUserPassword = 'ejemplo';
+  newUserEmail = 'ejemplo@falso.com';
+  newUserRealName = 'ejemplo';
+  newUserRealLastName = 'falso';
+  newUserType = 'admin';
 
-    userTypes = ['admin', 'godfather'];
+  userTypes = ['admin', 'godfather'];
 
-    name = '';
+  name = '';
 
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-    constructor(
-        private apiService: ApiService,
-        private router: Router,
-        private messageService: MessageService
-    ) {}
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
 
-    ngOnInit() {
-        this.refresh();
-    }
+  ngOnInit() {
+    this.refresh();
+  }
 
-    refresh() {
-        this.apiService.getUsers('', 0, this.pageSize).subscribe(
-            users => {
-                this.data = users.items;
-                this.sort.sortChange.subscribe(
-                    () => (this.paginator.pageIndex = 0)
-                );
+  refresh() {
+    this.apiService.getUsers('', 0, this.pageSize).subscribe(
+      users => {
+        this.data = users.items;
+        this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
 
-                merge(this.sort.sortChange, this.paginator.page)
-                    .pipe(
-                        startWith({}),
-                        switchMap(() => {
-                            this.isLoadingResults = true;
-                            return this.apiService.getUsers(
-                                this.name,
-                                this.paginator.pageIndex,
-                                this.paginator.pageSize
-                            );
-                        }),
-                        map(data => {
-                            this.isLoadingResults = false;
-                            this.resultsLength = data.total;
+        merge(this.sort.sortChange, this.paginator.page)
+          .pipe(
+            startWith({}),
+            switchMap(() => {
+              this.isLoadingResults = true;
+              return this.apiService.getUsers(
+                this.name,
+                this.paginator.pageIndex,
+                this.paginator.pageSize
+              );
+            }),
+            map(data => {
+              this.isLoadingResults = false;
+              this.resultsLength = data.total;
 
-                            return data.items;
-                        }),
-                        catchError(() => {
-                            this.isLoadingResults = false;
-                            return observableOf([]);
-                        })
-                    )
-                    .subscribe(data => (this.data = data));
-            },
-            error => {
-                this.isLoadingResults = false;
-                this.messageService.show('Error cargando los usuarios');
-            }
-        );
-    }
+              return data.items;
+            }),
+            catchError(() => {
+              this.isLoadingResults = false;
+              return observableOf([]);
+            })
+          )
+          .subscribe(data => (this.data = data));
+      },
+      error => {
+        this.isLoadingResults = false;
+        this.messageService.show('Error cargando los usuarios');
+      }
+    );
+  }
 
-    newUser() {
-        this.apiService
-            .newUser(
-                this.newUserName,
-                this.newUserPassword,
-                this.newUserEmail,
-                this.newUserRealName,
-                this.newUserRealLastName
-            )
-            .subscribe(
-                () => {
-                    this.messageService.show('Usuario creado con éxito');
-                    this.refresh();
-                    this.creatingNewElement = false;
+  newUser() {
+    this.apiService
+      .newUser(
+        this.newUserName,
+        this.newUserPassword,
+        this.newUserEmail,
+        this.newUserRealName,
+        this.newUserRealLastName
+      )
+      .subscribe(
+        () => {
+          this.messageService.show('Usuario creado con éxito');
+          this.refresh();
+          this.creatingNewElement = false;
 
-                    this.newUserName = 'ejemplo';
-                    this.newUserPassword = 'ejemplo';
-                    this.newUserEmail = 'ejemplo@falso.com';
-                },
-                () => this.messageService.show('Error creando el usuario')
-            );
-    }
+          this.newUserName = 'ejemplo';
+          this.newUserPassword = 'ejemplo';
+          this.newUserEmail = 'ejemplo@falso.com';
+        },
+        () => this.messageService.show('Error creando el usuario')
+      );
+  }
 
-    confirmUserDeletion(user: User) {
-        this.toBeDeleted = user;
-        this.messageService.show(
-            'Seguro que desea eliminar?',
-            'Sí',
-            this,
-            'deleteUser'
-        );
-    }
+  confirmUserDeletion(user: User) {
+    this.toBeDeleted = user;
+    this.messageService.show(
+      'Seguro que desea eliminar?',
+      'Sí',
+      this,
+      'deleteUser'
+    );
+  }
 
-    async deleteUser() {
-        await this.apiService.deleteUser(this.toBeDeleted.id).subscribe(
-            () => {
-                this.messageService.show('Usuario eliminado');
-            },
-            () => {
-                this.messageService.show('Error eliminando');
-            }
-        );
-        this.paginator.pageIndex = 0;
-        this.refresh();
-    }
+  async deleteUser() {
+    await this.apiService.deleteUser(this.toBeDeleted.id).subscribe(
+      () => {
+        this.messageService.show('Usuario eliminado');
+      },
+      () => {
+        this.messageService.show('Error eliminando');
+      }
+    );
+    this.paginator.pageIndex = 0;
+    this.refresh();
+  }
 
-    logOut() {
-        this.apiService.logOut();
-    }
+  logOut() {
+    this.apiService.logOut();
+  }
 
-    selectUser(user: User) {
-        this.expandedElement = user;
-        this.expandedUserName = this.expandedElement.name;
-        this.expandedUserEmail = this.expandedElement.email;
-        this.expandedUserPassword = this.expandedElement.password;
-        this.expandedUserEnabled = this.expandedElement.enabled || false;
-        this.expandedUserType = this.expandedElement.type;
+  selectUser(user: User) {
+    this.expandedElement = user;
+    this.expandedUserName = this.expandedElement.name;
+    this.expandedUserEmail = this.expandedElement.email;
+    this.expandedUserPassword = this.expandedElement.password;
+    this.expandedUserEnabled = this.expandedElement.enabled || false;
+    this.expandedUserType = this.expandedElement.type;
+    this.editting = false;
+  }
+
+  deselectUser() {
+    this.expandedElement = undefined;
+    this.expandedUserName = undefined;
+    this.expandedUserEmail = undefined;
+    this.expandedUserPassword = undefined;
+    this.expandedUserEnabled = undefined;
+    this.expandedUserRealLastName = undefined;
+    this.expandedUserRealName = undefined;
+    this.expandedUserType = undefined;
+  }
+
+  saveUser() {
+    const u: User = new User();
+    u.name = this.expandedUserName;
+    u.password = this.expandedUserPassword;
+    u.enabled = this.expandedUserEnabled;
+    u.email = this.expandedUserEmail;
+    u.id = this.expandedElement.id;
+    this.apiService.editUser(u).subscribe(
+      () => {
         this.editting = false;
-    }
-
-    deselectUser() {
-        this.expandedElement = undefined;
-        this.expandedUserName = undefined;
-        this.expandedUserEmail = undefined;
-        this.expandedUserPassword = undefined;
-        this.expandedUserEnabled = undefined;
-        this.expandedUserRealLastName = undefined;
-        this.expandedUserRealName = undefined;
-        this.expandedUserType = undefined;
-    }
-
-    saveUser() {
-        const u: User = new User();
-        u.name = this.expandedUserName;
-        u.password = this.expandedUserPassword;
-        u.enabled = this.expandedUserEnabled;
-        u.email = this.expandedUserEmail;
-        u.id = this.expandedElement.id;
-        this.apiService.editUser(u).subscribe(
-            () => {
-                this.editting = false;
-                this.deselectUser();
-                this.refresh();
-                this.messageService.show('Usuario editado');
-            },
-            () => {
-                this.messageService.show('Error editando');
-            }
-        );
-    }
+        this.deselectUser();
+        this.refresh();
+        this.messageService.show('Usuario editado');
+      },
+      () => {
+        this.messageService.show('Error editando');
+      }
+    );
+  }
 }
