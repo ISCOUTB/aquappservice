dcae78f375f81d508c1e0f5538ed18070f1b9711
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-04-19 13:40:05 -0500

    Url service is now a part of the url module

---
+++ aquapp/src/app/modules/utils/url/url.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UrlService } from './services/url/url.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [UrlService]
+})
+export class UrlModule {}
